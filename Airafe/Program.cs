f278ebb58369f22f91797fc6cecc882d31ae11a6
Frank Chen <newbeev5@outlook.com>
2017-08-20 18:16:04 +0800

    名称空间“AriSafe”封装航安公司常用功能、控件优化及部分产品相关功能。本名称空间长期维护。

---
+++ Airafe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*       说明
+名称空间“AriSafe”包含了所以航安公司常用的功能、控件及部分产品相关的代码段封装。
+本名称空间长期维护、长期有效。
+
+*/
+namespace AirSafe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class FunctionPrintClass
+    {
+        
+    }
+    class FunctionSQLServer
+    {
+        
+    }
+    class FunctionSerialCommunation
+    {
+        
+    }
+    class ProductPAPI
+    {
+        
+    }
+    class ProductSFLS
+    {
+        
+    }
+}
